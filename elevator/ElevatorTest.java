c888649ecd05ecbd3fc957c1686f5f10f98b2eba
Eduin Camilo Restrepo <eduinrc@gmail.com>
2020-08-18 21:42:28 -0500

    Ejecicios clase 3

--- elevator/ElevatorTest.java
+++ elevator/ElevatorTest.java
@@ -1,14 +1,49 @@
 public class ElevatorTest{
 
 	public static void main(String args[]){
+		//GIVEN
 		Elevator myElevator = new Elevator();
+		
+		//WHEN
 		myElevator.goUp();
 		myElevator.goUp();
 		myElevator.goUp();
 		myElevator.goUp();
-		System.out.println("The Elevator is on floor " + myElevator.currentFloor);
-		myElevator.goDown();
-		System.out.println("The Elevator is on floor " + myElevator.currentFloor);
+		myElevator.goUp();
+		myElevator.goUp();
+		myElevator.goUp();
+		myElevator.goUp();
+		myElevator.goUp();
+		myElevator.goUp();
+		myElevator.goUp();
+		myElevator.goUp();
+		myElevator.goUp();
+		myElevator.goUp();
+
+		//THEN
+		if(myElevator.currentFloor <= myElevator.topFloor){
+			System.out.println("---- VALIDATION 1---\nOk");
+		}
+		else{
+			System.out.println("---- VALIDATION 1 ---\nNot Ok - The elevator is over the topFloor");		
+		}
+
+		//GIVEN
+		myElevator.currentFloor = 1;
+		myElevator.openDoor();
+
+		//WHEN
+		myElevator.goUp();
+
+		//THEN
+		if(myElevator.currentFloor == 1){
+			System.out.println("---- VALIDATION 2 ---\nOk");
+		}
+		else{
+			System.out.println("---- VALIDATION 2 ---\nNot Ok - The elevator moves with the door open");		
+		}
+
+		
 	}
 
 }
