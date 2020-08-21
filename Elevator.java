2cfdf3d29e8f124085d04c12c90f18a0bf6ff123
chikyulieth <57535257+chikyulieth@users.noreply.github.com>
2020-08-20 23:59:55 -0500

    Add files via upload

---
+++ Elevator.java
@@ -0,0 +1,21 @@
+public class Elevator{
+	public int currentFloor = 1;
+	public int desiredFloor;
+	public int topFloor;
+
+	public void goUp(){
+		System.out.println("Going up...");
+		currentFloor++;	
+	}
+
+	public void goDown(){
+		System.out.println("Going down...");
+		currentFloor--;	
+	}
+
+	public void openDoor(){
+
+		System.out.println("door opening");
+	}
+	
+}
